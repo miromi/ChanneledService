923556045dcaf303fa7f8926d894118f36c7f7a4
Ray Yao <miromi1983@gmail.com>
2011-10-22 00:28:42 +0800

    add abstract interface of channel and input_channel

---
+++ channel.h
@@ -0,0 +1,11 @@
+#ifndef _CHANNEL_H_DEF_
+#define _CHANNEL_H_DEF_
+
+class channel {
+public:
+	channel(){};
+	virtual ~channel(){};
+};
+
+#endif
+
---
+++ input_channel.h
@@ -0,0 +1,14 @@
+#ifndef _INPUT_CHANNEL_H_DEF_
+#define _INPUT_CHANNEL_H_DEF_
+
+class input_channel : public channel {
+public:
+	input_channel(){
+  };
+	virtual ~input_channel(){
+  };
+
+};
+
+#endif
+
